36dda5d8e82d43c9e8c99f03670cab678ee30ca2
nattasit phosongchan <nattarsit@gmail.com>
2022-01-01 16:05:32 +0700

    add todos migration

---
+++ database/migrations/2022_01_01_084239_create_todos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('todo_txt')->collation('utf8_general_ci')->charset('utf8');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
